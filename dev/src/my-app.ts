b79e7bab12559dc783d470f605c6aa4550dc8aa1
runem <runemehlsen@gmail.com>
2019-02-10 16:16:45 +0100

    Update dependencies

--- dev/src/my-app.ts
+++ dev/src/my-app.ts
@@ -1,8 +1,8 @@
 import { html, render } from "lit-html";
 import "./my-button";
 
-const myApp = (text: string, myNumber: number) => html`
-	<my-button maxlength="${myNumber}" class="${text}" color="purple" @click="${console.log}" disabled text="${text}"></my-button>
+const myApp = (text: string, disabled: boolean) => html`
+	<my-button size="large" text="${text}" ?disabled="${disabled}"></my-button>
 `;
 
-render(myApp("Hello", 1), document.body);
+render(myApp("Hello", false), document.body);
--- dev/src/my-button.ts
+++ dev/src/my-button.ts
@@ -1,16 +1,14 @@
 import { customElement, property, html, LitElement } from "lit-element";
-import { classMap } from "lit-html/directives/class-map";
 
 @customElement("my-button")
 export class MyButton extends LitElement {
-	@property({ type: String }) color: "red" | "purple" | undefined;
-	@property({ type: String }) text: string = "";
-	@property({ type: String }) disabled: boolean = false;
-	@property({ type: String }) maxlength: number | undefined;
+	@property({ type: String }) size!: "small" | "medium" | "large";
+	@property({ type: Number }) text: string = "";
+	@property({ type: Boolean }) disabled: boolean = false;
 
 	render() {
 		return html`
-			<button class="${classMap({ foo: true })}">${this.text}</button>
+			<button>${this.text}</button>
 		`;
 	}
 }
