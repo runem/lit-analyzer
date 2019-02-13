a156655fa314b134f74debee318eba21f8f66023
andreasbm <andmehlsen@gmail.com>
2019-02-13 10:40:00 +0100

    Added functionality to get the intersecting html attribute value.

--- src/diagnostics/get/get-completions-from-position.ts
+++ src/diagnostics/get/get-completions-from-position.ts
@@ -2,9 +2,10 @@ import { CompletionEntry, CompletionInfo } from "typescript";
 import { CssDocument } from "../../parsing/text-document/css-document/css-document";
 import { HtmlDocument } from "../../parsing/text-document/html-document/html-document";
 import { TextDocument } from "../../parsing/text-document/text-document";
-import { IHtmlNodeAttrBase } from "../../types/html-node-attr-types";
+import { HtmlNodeAttr } from "../../types/html-node-attr-types";
 import { HtmlNode } from "../../types/html-node-types";
 import { DocumentPositionContext } from "../../util/get-html-position";
+import { logger } from "../../util/logger";
 import { intersects } from "../../util/util";
 import { VscodeCssServiceWrapper } from "../css-document/vscode-css-languageservice/vscode-css-service-wrapper";
 import { DiagnosticsContext } from "../diagnostics-context";
@@ -17,22 +18,25 @@ export function getCompletionInfoFromPosition(document: TextDocument, positionCo
 		return new VscodeCssServiceWrapper(document).getCompletionInfoAtPosition(positionContext.positionInText);
 	} else if (document instanceof HtmlDocument) {
 		// Get possible intersecting html attribute or attribute area.
-		const htmlAttr = getIntersectingHtmlAttrName(document.rootNodes, position) || undefined;
+		const htmlAttrName = getIntersectingHtmlAttrName(document.rootNodes, position) || undefined;
+		const htmlAttrValue = getIntersectingHtmlAttrValue(document.rootNodes, position);
 		const insideAttrAreaNode = getIntersectingHtmlNodeAttrArea(document.rootNodes, position);
 
 		// Get entries from the extensions
 		let entries: CompletionEntry[] | undefined = [];
-		if (htmlAttr != null) {
-			entries = completionsForHtmlAttrs(htmlAttr.htmlNode, positionContext, context);
+		if (htmlAttrName != null) {
+			entries = completionsForHtmlAttrs(htmlAttrName.htmlNode, positionContext, context);
 
 			if (entries != null) {
 				// Make sure that every entry overwrites the entire attribute name.
-				const { start, end } = htmlAttr.location.name;
+				const { start, end } = htmlAttrName.location.name;
 				entries = entries.map(entry => ({
 					...entry,
 					replacementSpan: { start, length: end - start }
 				}));
 			}
+		} else if (htmlAttrValue != null && ['"', "'", "="].includes(beforeWord)) {
+			logger.debug(htmlAttrValue);
 		} else if (insideAttrAreaNode != null) {
 			entries = completionsForHtmlAttrs(insideAttrAreaNode, positionContext, context);
 		} else if (beforeWord === "<") {
@@ -52,6 +56,22 @@ export function getCompletionInfoFromPosition(document: TextDocument, positionCo
 }
 
 /**
+ * Traverses the html nodes and returns the first match if any exists.
+ * @param htmlNode
+ * @param position
+ * @param traverser
+ */
+function traverseHtmlNodes<T>(htmlNode: HtmlNode[], position: number, traverser: (htmlNode: HtmlNode | HtmlNode[], position: number) => T | undefined): T | undefined {
+	// Loop through all nodes in the array. Stop if a result is encountered.
+	for (const childNode of htmlNode || []) {
+		const res = traverser(childNode, position);
+		if (res != null) return res;
+	}
+
+	return undefined;
+}
+
+/**
  * Loop through a htmlNode or more in order to * find out
  * if the position is inside the part of the start tag where
  * attributes are specified.
@@ -60,29 +80,23 @@ export function getCompletionInfoFromPosition(document: TextDocument, positionCo
  */
 function getIntersectingHtmlNodeAttrArea(htmlNode: HtmlNode | HtmlNode[], position: number): HtmlNode | undefined {
 	if (Array.isArray(htmlNode)) {
-		// Loop through all nodes in the array. Stop if a result is encountered.
-		for (const childNode of htmlNode || []) {
-			const res = getIntersectingHtmlNodeAttrArea(childNode, position);
-			if (res != null) return res;
-		}
-
-		return undefined;
-	} else {
-		// Tests if the position is inside the start tag
-		outer: if (position > htmlNode.location.name.end && intersects(position, htmlNode.location.startTag)) {
-			// Check if the position intersects any attributes. Break if so.
-			for (const htmlAttr of htmlNode.attributes) {
-				if (intersects(position, htmlAttr.location)) {
-					break outer;
-				}
-			}
-
-			return htmlNode;
-		}
-
-		// Check recursively on all child nodes.
-		return getIntersectingHtmlNodeAttrArea(htmlNode.children || [], position);
+		return traverseHtmlNodes(htmlNode, position, getIntersectingHtmlNodeAttrArea);
 	}
+
+	// Tests if the position is inside the start tag
+	outer: if (position > htmlNode.location.name.end && intersects(position, htmlNode.location.startTag)) {
+		// Check if the position intersects any attributes. Break if so.
+		for (const htmlAttr of htmlNode.attributes) {
+			if (intersects(position, htmlAttr.location)) {
+				break outer;
+			}
+		}
+
+		return htmlNode;
+	}
+
+	// Check recursively on all child nodes.
+	return getIntersectingHtmlNodeAttrArea(htmlNode.children || [], position);
 }
 
 /**
@@ -90,24 +104,43 @@ function getIntersectingHtmlNodeAttrArea(htmlNode: HtmlNode | HtmlNode[], positi
  * @param htmlNode
  * @param position
  */
-function getIntersectingHtmlAttrName(htmlNode: HtmlNode | HtmlNode[], position: number): IHtmlNodeAttrBase | undefined {
+function getIntersectingHtmlAttrName(htmlNode: HtmlNode | HtmlNode[], position: number): HtmlNodeAttr | undefined {
 	if (Array.isArray(htmlNode)) {
-		// Loop through all nodes in the array. Stop if a result is encountered.
-		for (const childNode of htmlNode || []) {
-			const res = getIntersectingHtmlAttrName(childNode, position);
-			if (res != null) return res;
-		}
+		return traverseHtmlNodes(htmlNode, position, getIntersectingHtmlAttrName);
+	}
 
-		return undefined;
-	} else {
-		// Loop through all attributes of this element.
-		// Tests if position intersects with the part of the attribute before "=".
-		for (const htmlAttr of htmlNode.attributes || []) {
-			if (intersects(position, htmlAttr.location.name)) {
+	// Loop through all attributes of this element.
+	// Tests if position intersects with the part of the attribute before "=".
+	for (const htmlAttr of htmlNode.attributes || []) {
+		if (intersects(position, htmlAttr.location.name)) {
+			return htmlAttr;
+		}
+	}
+
+	return getIntersectingHtmlAttrName(htmlNode.children || [], position);
+}
+
+/**
+ * Returns a htmlAttrValue that intersects with position.
+ * @param htmlNode
+ * @param position
+ */
+function getIntersectingHtmlAttrValue(htmlNode: HtmlNode | HtmlNode[], position: number): HtmlNodeAttr | undefined {
+	if (Array.isArray(htmlNode)) {
+		return traverseHtmlNodes(htmlNode, position, getIntersectingHtmlAttrValue);
+	}
+
+	// Loop through all attributes of this element.
+	// Tests if position intersects with the part of the attribute before "=".
+	for (const htmlAttr of htmlNode.attributes || []) {
+		if (htmlAttr.assignment != null) {
+			const start = htmlAttr.location.name.end;
+			const end = htmlAttr.location.end;
+			if (intersects(position, { start, end })) {
 				return htmlAttr;
 			}
 		}
-
-		return getIntersectingHtmlAttrName(htmlNode.children || [], position);
 	}
+
+	return getIntersectingHtmlAttrValue(htmlNode.children || [], position);
 }
