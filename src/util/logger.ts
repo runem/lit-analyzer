475199eb36bb79a0439baa1e56cc4ded4d3c2ffd
runem <runemehlsen@gmail.com>
2019-03-30 02:03:46 +0100

    Remove log statement

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,7 +2,7 @@ import { appendFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import { inspect } from "util";
 
-//const logPath = join(__dirname, "../../log.txt");
+//const logPath = join(__dirname, "../../lit-plugin.log");
 const logPath = join(process.cwd(), "lit-plugin.log");
 
 export enum LoggingLevel {
