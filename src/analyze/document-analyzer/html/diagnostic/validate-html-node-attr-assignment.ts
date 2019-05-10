c9acf1a980edf6eb46ed751b8f3102b6363ea9aa
runem <runemehlsen@gmail.com>
2019-05-10 19:46:01 +0200

    Make it possible to use an object literal with 'handleEvent' property to bind in an @event listener binding

--- src/analyze/document-analyzer/html/diagnostic/validate-html-node-attr-assignment.ts
+++ src/analyze/document-analyzer/html/diagnostic/validate-html-node-attr-assignment.ts
@@ -115,12 +115,7 @@ function validateHtmlAttrAssignmentRules(htmlAttr: HtmlNodeAttr, typeB: SimpleTy
 		case HtmlNodeAttrKind.EVENT_LISTENER:
 			// Make sure that there is a function as event listener value.
 			// Here we catch errors like: @click="onClick()"
-			if (
-				!isAssignableToSimpleTypeKind(typeB, [SimpleTypeKind.FUNCTION, SimpleTypeKind.METHOD, SimpleTypeKind.UNKNOWN], {
-					matchAny: true,
-					op: "or"
-				})
-			) {
+			if (!isTypeBindableToEventListener(typeB)) {
 				return [
 					{
 						kind: LitHtmlDiagnosticKind.NO_EVENT_LISTENER_FUNCTION,
@@ -658,4 +653,33 @@ export function relaxType(type: SimpleType): SimpleType {
 		default:
 			return type;
 	}
+}
+
+/**
+ * Returns if this type can be used in a event listener binding
+ * @param type
+ */
+function isTypeBindableToEventListener(type: SimpleType): boolean {
+	// Callable types can be used in the binding
+	if (
+		isAssignableToSimpleTypeKind(type, [SimpleTypeKind.FUNCTION, SimpleTypeKind.METHOD, SimpleTypeKind.UNKNOWN], {
+			matchAny: true,
+			op: "or"
+		})
+	) {
+		return true;
+	}
+
+	// Object types with attributes for the setup function of the event listener can be used
+	if (type.kind === SimpleTypeKind.OBJECT || type.kind === SimpleTypeKind.INTERFACE) {
+		// The "handleEvent" property must be present
+		const handleEventFunction = type.members != null ? type.members.find(m => m.name === "handleEvent") : undefined;
+
+		// The "handlEvent" property must be callable
+		if (handleEventFunction != null) {
+			return isTypeBindableToEventListener(handleEventFunction.type);
+		}
+	}
+
+	return false;
 }
