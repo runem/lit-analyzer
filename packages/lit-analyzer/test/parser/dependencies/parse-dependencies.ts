041aece51503f6b119184cb904edc4d941f5f12d
James Garbutt <43081j@users.noreply.github.com>
2021-05-09 18:31:10 +0100

    skip type-only exports too

--- packages/lit-analyzer/test/parser/dependencies/parse-dependencies.ts
+++ packages/lit-analyzer/test/parser/dependencies/parse-dependencies.ts
@@ -300,3 +300,24 @@ tsTest("Ignores type-only imports in a file", t => {
 
 	t.deepEqual(sortedFileNames, ["file2.ts"]);
 });
+
+tsTest("Ignores type-only exports in a file", t => {
+	const { sourceFile, context } = prepareAnalyzer([
+		{ fileName: "file1.ts", text: `` },
+		{
+			fileName: "file2.ts",
+			text: `
+				export type { MyElement } from "./file1";
+			`,
+			entry: true
+		}
+	]);
+
+	const dependencies = parseAllIndirectImports(sourceFile, context);
+
+	const sortedFileNames = Array.from(dependencies)
+		.map(file => file.fileName)
+		.sort();
+
+	t.deepEqual(sortedFileNames, ["file2.ts"]);
+});
