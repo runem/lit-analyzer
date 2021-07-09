9de34d91db3fe0df206269e746d02a93eaba84ef
Justin Fagnani <justinfagnani@google.com>
2021-07-09 15:54:29 -0700

    [lit-analyzer] Fix linting

--- packages/lit-analyzer/src/scripts/check-version.ts
+++ packages/lit-analyzer/src/scripts/check-version.ts
@@ -8,6 +8,7 @@ const constantsPath = path.resolve("src/lib/analyze/constants.ts");
 const constantsSource = fs.readFileSync(constantsPath, "utf-8");
 
 if (!constantsSource.includes(`"${version}"`)) {
+	// eslint-disable-next-line no-console
 	console.log(`\nExpected src/lib/analyze/constants.ts to contain the current version "${version}"`);
 	process.exit(1);
 }
