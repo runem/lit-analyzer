a972119d769672548b3eb7edb9ab5fcccfc92bce
Peter Burns <rictic@google.com>
2022-04-13 18:19:19 -0700

    Better document copy-to-built

--- packages/vscode-lit-plugin/copy-to-built.js
+++ packages/vscode-lit-plugin/copy-to-built.js
@@ -1,20 +1,39 @@
 const { copy, mkdirp, writeFile } = require("fs-extra");
+
+/**
+ * Copy files into the ./built directory.
+ *
+ * This is the directory that actually has the final filesystem layout for
+ * the extension, and to keep the vsix file small we want to only include
+ * those files that are needed.
+ *
+ * Note that ./built/bundle.js is generated directly by esbuild.script.js and
+ * not copied by this script.
+ */
 async function main() {
+	// We don't bundle the typescript compiler into ./built/bundle.js, so we need
+	// a copy of it.
+	await copy("./node_modules/typescript", "./built/node_modules/typescript");
+
+	// For the TS compiler plugin, it must be in node modules because that's
+	// hard coded by the TS compiler's custom module resolution logic.
 	await mkdirp("./built/node_modules/ts-lit-plugin/lib");
 	const tsPluginPackageJson = require("../ts-lit-plugin/package.json");
-	// Needed because we're using the bundle, so all deps have been packaged
-	// in.
+	// We're only using the bundled version, so the plugin doesn't need any
+	// dependencies.
 	tsPluginPackageJson.dependencies = {};
 	await writeFile("./built/node_modules/ts-lit-plugin/package.json", JSON.stringify(tsPluginPackageJson, null, 2));
 	await copy("../ts-lit-plugin/index.js", "./built/node_modules/ts-lit-plugin/index.js");
 	await copy("../ts-lit-plugin/lib/bundle-esbuild.js", "./built/node_modules/ts-lit-plugin/lib/bundle-esbuild.js");
 
-	await copy("./node_modules/typescript", "./built/node_modules/typescript");
-
 	const pluginPackageJson = require("./package.json");
-	// Needed because vsce publish verifies the node_module directory
+	// vsce is _very_ picky about the directories in node_modules matching the
+	// extension's package.json, so we need an entry for ts-lit-plugin or it
+	// will think that it's extraneous.
 	pluginPackageJson.dependencies["ts-lit-plugin"] = "^v1.0.0-fake-version";
 	await writeFile("./built/package.json", JSON.stringify(pluginPackageJson, null, 2));
+
+	// Copy static files used by the extension.
 	await copy("./LICENSE.md", "./built/LICENSE.md");
 	await copy("./README.md", "./built/README.md");
 	await copy("./docs", "./built/docs");
