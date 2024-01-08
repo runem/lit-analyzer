53f7b563d01058ea19b4eef8ca3efbf6d671b5a5
Andrew Jakubowicz <ajakubowicz@google.com>
2024-01-08 10:51:40 -0800

    add regression test and fix self closing custom-element in another test

--- packages/lit-analyzer/src/test/rules/no-unclosed-tag.ts
+++ packages/lit-analyzer/src/test/rules/no-unclosed-tag.ts
@@ -30,6 +30,14 @@ tsTest("Report unclosed 'p' tag that is implicitly closed via tag omission conta
 	hasDiagnostic(t, diagnostics, "no-unclosed-tag");
 });
 
+// Regeression test for https://github.com/runem/lit-analyzer/issues/283
+tsTest("Report 'p' tag that is implicitly closed via tag omission containing a space", t => {
+	// Note, the browser will parse this case into: `<p> </p><div></div><p></p>` which can be
+	// unexpected, but technically means the first `<p>` tag is not explicitly closed.
+	const { diagnostics } = getDiagnostics("html`<p> <div></div></p>`", { rules: { "no-unclosed-tag": true } });
+	hasDiagnostic(t, diagnostics, "no-unclosed-tag");
+});
+
 // Self-closing tags do not exist in HTML. They are only valid in SVG and MathML.
 tsTest("Report non-void element using self closing syntax", t => {
 	const { diagnostics } = getDiagnostics("html`<p /><div></div>`", { rules: { "no-unclosed-tag": true } });
